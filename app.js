51f81f480836f209722e2c23715ed240f55a554d
NEZ\DESKTOP <elsword485667@gmail.com>
2024-05-25 02:49:57 +0900

    [update]

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const indexRouter = require('./routes/index')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+const cors = require("cors");
+require("dotenv").config();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
 const app = express();
-app.use(bodyParser.json());
-app.use('/api', indexRouter)
-app.use(express.urlencoded({extended: true}))
 
-const mongoUrI = 'mongodb://127.0.0.1:27017/todo-demo'
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", indexRouter);
+app.use(express.urlencoded({ extended: true }));
+
+const mongoUrI = MONGODB_URI_PROD;
 
 mongoose
-    .connect(mongoUrI, {useNewUrlParser: true})
+    .connect(mongoUrI, { useNewUrlParser: true })
     .then(() => {
-        console.log('mongoose connected')
+        console.log("mongoose connected");
     })
     .catch((err) => {
-        console.log(err)
-    })
+        console.log(err);
+    });
 
-app.listen(3000, () => {
-    console.log('server connected')
-})
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server connected");
+});
--- model/Task.js
+++ model/Task.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const taskSchema = Schema({
     task: {
         type: String,
-        required: true
+        required: true,
     },
     isComplete: {
         type: Boolean,
-        required: true
-    }
-})
+        required: true,
+    },
+});
 
-const Task = mongoose.model('Task', taskSchema)
+const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
