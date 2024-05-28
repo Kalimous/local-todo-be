d3cfc09d6d15c006660bb1045c8e4f200af43457
NEZ\DESKTOP <elsword485667@gmail.com>
2024-05-28 22:42:05 +0900

    1.0.0

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const cors = require("cors");
 require("dotenv").config();
-const MONGODB_LOCAL_URI_PROD = process.env.MONGODB_LOCAL_URI_PROD;
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 app.use(express.urlencoded({ extended: true }));
 
-const mongoUrI = MONGODB_LOCAL_URI_PROD;
+const mongoUrI = MONGODB_URI_PROD;
 
 mongoose
     .connect(mongoUrI)
@@ -24,6 +25,6 @@ mongoose
         console.log(err);
     });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("server connected");
+app.listen(port, () => {
+    console.log(`server connected on ${port}`);
 });
